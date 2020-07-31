b83ee968f5d36f11aa0ec234795841b2298f2f27
tamirazrab <tamirazrab@gmail.com>
2020-07-31 11:59:43 +0500

    Drawing horizontals on canvas

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { world } = engine;
 const cells = 3; // As grid is square 3x3, making one variable would be enough for now.
 const width = 600;
 const height = 600;
+const unitLength = width / cells; // As width and height are same for now that's why.
 
 const render = Render.create({
     element: document.body,
@@ -18,7 +19,6 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-
 let walls = [
     Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
     Bodies.rectangle(width / 2, 600, width, 40, { isStatic: true }),
@@ -29,6 +29,20 @@ let walls = [
 World.add(world, walls);
 
 // Generating Maze
+
+let shuffle = (arr) => {
+    let length = arr.length;
+    while (length > 0) {
+        const index = Math.floor(Math.random() * length);
+        length--;
+        let temp = arr[length];
+        arr[length] = arr[index];
+        arr[index] = temp;
+
+    }
+    return arr;
+};
+
 const grid = Array(cells)
     .fill(null)
     .map(() => Array(cells).fill(false));
@@ -56,12 +70,48 @@ const goThroughMaze = (row, col) => {
 
     // Calculate position for neighbors of current position.
     // It'd be in format of UP, RIGHT, BOTTOM, LEFT.
-    const neighbors = [
-        [row - 1, col],
-        [row, col + 1],
-        [row + 1, col],
-        [row, col - 1]
-    ];
+    // Don't know why shuffling this, I thought it would be
+    // constant for any position. 
 
+    const neighbors = shuffle([
+        [row - 1, col, 'up'],
+        [row, col + 1, 'right'],
+        [row + 1, col, 'down'],
+        [row, col - 1, 'left']
+    ]);
+    console.log(neighbors);
+    // Now to iterate over each neighbor 
+    for (let neighbor of neighbors) {
+        const [nextRow, nextCol, direction] = neighbor;
+
+        if (nextRow < 0 || nextRow >= cells || nextCol < 0 || nextCol >= cells || grid[nextRow][nextCol])
+            continue;
+
+        // Removing wall from either vertical or horizontals
+        switch (direction) {
+            case 'left': verticals[row][col - 1] = true; break;
+            case 'right': verticals[row][col] = true; break;
+            /* Anything greater than 1 in col will point the fact that we moved right so that's why just col - No it's because we will only remove walls of r0 and r1 can't touch r2 because it's border of maze. */
+            case 'up': horizontals[row - 1][col] = true; break;
+            case 'down': horizontals[row][col] = true; break;
+        }
+
+        // Visiting next cell
+        goThroughMaze(nextRow, nextCol);
+        // Eventually recursion to fill out the maze
+    }
 }
 
+goThroughMaze(startingRow, startingCol);
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+        /* col contains whether there should be any wall or not */
+        if (col)
+            return; // If there is no wall then return
+
+        const wall = Bodies.rectangle(colIndex * unitLength + unitLength / 2, rowIndex * unitLength + unitLength, unitLength, 4, { isStatic: true });
+
+        World.add(world, wall);
+    });
+});
