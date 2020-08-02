397109ba865bc6def5c049b60bf55b4af8c3aa09
tamirazrab <tamirazrab@gmail.com>
2020-08-02 13:32:39 +0500

    Fixed down broder line appearing at surface height

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ Runner.run(Runner.create(), engine);
 
 let walls = [
     Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
-    Bodies.rectangle(width / 2, 600, width, 2, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
     Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
     Bodies.rectangle(width, height / 2, 2, height, { isStatic: true })
 ];
